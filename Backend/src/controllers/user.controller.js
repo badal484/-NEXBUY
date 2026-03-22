184a7e64b31ab23d71a988c90e8b36f6eeddf508
badal484 <badal90603@gmail.com>
2026-03-22 21:55:33 +0530

    add user.controller.js

---
+++ Backend/src/controllers/user.controller.js
@@ -0,0 +1,11 @@
+import User from "../models/user.model.js";
+
+export const becomeSeller = async (req, res) => {
+  const user = await User.findByIdAndUpdate(
+    req.user.id,
+    { role: "seller" },
+    { new: true }
+  );
+
+  res.json(user);
+};
