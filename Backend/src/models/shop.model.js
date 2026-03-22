8cae8fa63f0e233a639d14b6b26eb3862968f936
KushguptaPST <kushg0082@gmail.com>
2026-03-22 21:51:19 +0530

    add shop&user model

---
+++ Backend/src/models/shop.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const shopSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  address: String,
+  isApproved: {
+    type: Boolean,
+    default: true // keep true for now (no admin approval yet)
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      default: "Point"
+    },
+    coordinates: [Number]
+  }
+});
+
+shopSchema.index({ location: "2dsphere" });
+
+export default mongoose.model("Shop", shopSchema);
+
+
+
