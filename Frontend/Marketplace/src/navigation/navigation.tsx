1480d0de3d201c22a0a9d6f9f2c6d526e08ec128
KushguptaPST <kushg0082@gmail.com>
2026-04-02 18:46:05 +0530

    add user login logic

--- Frontend/Marketplace/src/navigation/navigation.tsx
+++ Frontend/Marketplace/src/navigation/navigation.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-// import RegisterScreen from "../screens/auth/RegisterScreen";
-// import LoginScreen from "../screens/auth/LoginScreen";
+import RegisterScreen from "../screens/auth/RegisterScreen";
+import LoginScreen from "../screens/auth/LoginScreen";
 import HomeScreen from "../screens/home/HomeScreen";
 import AddProductScreen from "../screens/seller/AddProductScreen";
 
@@ -15,8 +15,8 @@ const Navigation = () => {
       <Stack.Navigator initialRouteName="Register">
         
         {/* Auth */}
-        {/* <Stack.Screen name="Register" component={RegisterScreen} />
-        <Stack.Screen name="Login" component={LoginScreen} /> */}
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
 
         {/* Main App */}
         <Stack.Screen name="Home" component={HomeScreen} />
